ee02c92cd846d8d6e95d7cd3becae8a24befcf03
YasinOkat <yasinokat@gmail.com>
2023-05-30 16:37:42 +0300

    Some language fixes

--- app/src/main/java/com/example/fitnesscalculator/MainActivity.kt
+++ app/src/main/java/com/example/fitnesscalculator/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.fitnesscalculator
 
+import android.content.SharedPreferences
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
@@ -9,18 +11,22 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.fitnesscalculator.R
+import androidx.preference.PreferenceManager
 import com.example.fitnesscalculator.databinding.ActivityMainBinding
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var sharedPrefs: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this)
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         val appBarConfiguration = AppBarConfiguration(
@@ -32,6 +38,8 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         binding.navView.setupWithNavController(navController)
+
+        setAppLanguageFromPreference()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -54,5 +62,30 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setAppLanguageFromPreference()
+    }
+
+    private fun setAppLanguageFromPreference() {
+        val language = sharedPrefs.getString("language", "en") // Default to English if language not found
+        setAppLanguage(language)
+    }
+
+    private fun setAppLanguage(language: String?) {
+        val locale = language?.let { Locale(it) }
+        if (locale != null) {
+            Locale.setDefault(locale)
+        }
+        val configuration = Configuration()
+        configuration.setLocale(locale)
+        baseContext.resources.updateConfiguration(configuration, baseContext.resources.displayMetrics)
+        saveLanguageToPreference(language)
+    }
+
+    private fun saveLanguageToPreference(language: String?) {
+        sharedPrefs.edit().putString("language", language).apply()
     }
 }
